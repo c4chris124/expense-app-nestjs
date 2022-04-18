703a282287cda8350a84e74c1b12c484c4dfd5a3
c4chris124 <c4chris124@gmail.com>
2022-04-18 17:34:33 -0600

    comments explaning how controller and method decorators works

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,25 @@
 import { Controller, Get } from "@nestjs/common"
 
 // All in nest js is a class, but to indicate this will be a controller (GET /example/something) we have to use the @Controller decorator
-@Controller()
+// @Controller()
+// @Controller('hi')
+// http://localhost:3333/if(controller decorator) + if(method decorators)
+// http://localhost:3333/hi/hello
+@Controller('report/income')
 export class AppController {
 
   // In order to indicate this will be a GET router we have to use the other decorator call @Get()
   // http://localhost:3333/ this is the get, theres is no path yet http://localhost:3333/NOTHINGYET
+  // @Get()
   @Get()
   getAllIncomeReports() {
+    return []
+  }
+
+
+  @Get()
+  getAllIncomeReports2() {
     return {}
   }
 }
+
