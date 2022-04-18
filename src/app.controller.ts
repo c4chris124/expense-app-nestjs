8d900dec2bafcc0d0cd11d6f61c2e7b03998cd2d
c4chris124 <c4chris124@gmail.com>
2022-04-18 17:25:17 -0600

    implementing the very firts controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common"
 
+// All in nest js is a class, but to indicate this will be a controller (GET /example/something) we have to use the @Controller decorator
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
+  // In order to indicate this will be a GET router we have to use the other decorator call @Get()
+  // http://localhost:3333/ this is the get, theres is no path yet http://localhost:3333/NOTHINGYET
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllIncomeReports() {
+    return {}
   }
 }
