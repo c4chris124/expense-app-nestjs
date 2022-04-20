bfcb8512d22884a24ec18be24d19035eac2ea6d4
c4chris124 <c4chris124@gmail.com>
2022-04-19 20:53:06 -0600

    Adding prittier to file, also adding some variables for Post or create report

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get, Post, Put, Delete, Param } from "@nestjs/common"
-import { data } from "./data";
-import { ReportType } from "./data";
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { data } from './data';
+import { ReportType } from './data';
 // All in nest js is a class, but to indicate this will be a controller (GET /example/something) we have to use the @Controller decorator
 // @Controller()
 // @Controller('hi')
@@ -8,39 +16,38 @@ import { ReportType } from "./data";
 // http://localhost:3333/hi/hello
 @Controller('report/:type')
 export class AppController {
-
   // In order to indicate this will be a GET router we have to use the other decorator call @Get()
   // http://localhost:3333/ this is the get, theres is no path yet http://localhost:3333/NOTHINGYET
   // @Get()
   @Get()
-  getAllReports(
-    @Param('type') type: string
-  ) {
-    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
-    return data.report.filter((report) => report.type === reportType)
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
   }
-
 
   @Get(':id')
-  getReportById() {
-    return {}
+  getReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
   }
 
-
   @Post()
-  createReport() {
-    return 'Created'
+  createReport(@Body() body: { amount: number; source: string }) {
+    return 'Created';
   }
 
   @Put(':id')
   updateReport() {
-    return 'Updated'
+    return 'Updated';
   }
 
   @Delete(':id')
   deleteReport() {
-    return 'Deleted'
+    return 'Deleted';
   }
-
 }
-
